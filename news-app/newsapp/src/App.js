c2dd5ac25ec8008b836eaac8e718b5a119a99fad
api-code <madavi.apekshas@gmail.com>
2024-03-12 19:30:26 +0530

    the news app is added with version 1

---
+++ news-app/newsapp/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+import React, { Component } from 'react'
+import NavBar from './Component/NavBar';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <NavBar/>
+      </div>
+    )
+  }
+}
+
